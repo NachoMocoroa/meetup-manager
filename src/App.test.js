f67c41410f3a1140e13d733c19fe306c72545ccb
Nacho <nachomocoroa@gmail.com>
2022-06-01 17:13:09 +0200

    App.test fix the component is wrapped in a <Provider>

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 /* eslint-disable testing-library/await-async-query */
 /* eslint-disable testing-library/no-debugging-utils */
 import { shallow, mount } from "enzyme";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 import App from "./App";
 import MainNavigation from "./components/layout/MainNavigation";
 import Layout from "./components/layout/Layout";
@@ -10,21 +12,21 @@ import Layout from "./components/layout/Layout";
  * @function setup
  * @returns {ShallowWrapper}
  */
-const setup = () => shallow(<App />);
-const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test]='${val}'`);
+//const setup = () => shallow(<App />);
+const setupProvider = (ui) => shallow(<Provider store={store}>{ui}</Provider>);
+//const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test]='${val}'`);
 
 test("renders App without crashing", () => {
-  const wrapper = setup();
-  //console.log(wrapper.debug());
+  const wrapper = setupProvider(<App />);
   expect(wrapper.exists()).toBe(true);
 });
 
 test("renders the navigation component", () => {
-  const wrapper = setup();
+  const wrapper = setupProvider(<App><MainNavigation /></App>);
   expect(wrapper.find(MainNavigation).length).toBe(1);
 });
 
 test("renders the Layout component", () => {
-  const wrapper = setup();
+  const wrapper = setupProvider(<App><Layout /></App>);
   expect(wrapper.find(Layout).length).toBe(1);
 });
