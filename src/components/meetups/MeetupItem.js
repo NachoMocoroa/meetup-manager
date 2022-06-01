901c96fc8cbdcdf39be478f66093c2c83b617edb
Nacho <nachomocoroa@gmail.com>
2022-06-01 16:52:33 +0200

    MeetupItem undefined content fix

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -10,19 +10,23 @@ export default function MeetupItem({ item, action, disabled, handleClick }) {
   return (
     <li className={classes.item} data-test='meet-up-item'>
       <Card>
-        <div className={classes.image}>
-          <img src={item.image} alt={item.title} />
-        </div>
-        <div className={classes.content}>
-          <h3>{item.title}</h3>
-          <address>{item.address}</address>
-          <p>{item.description}</p>
-        </div>
-        <div className={classes.actions}>
-          <button onClick={handleClick} disabled={disabled}>
-            {action === 'add' ? addFavoriteText : removeFavoriteText}
-          </button>
-        </div>
+        {item && (
+          <>
+            <div className={classes.image}>
+              <img src={item.image} alt={item.title} />
+            </div>
+            <div className={classes.content}>
+              <h3>{item.title}</h3>
+              <address>{item.address}</address>
+              <p>{item.description}</p>
+            </div>
+            <div className={classes.actions}>
+              <button onClick={handleClick} disabled={disabled}>
+                {action === 'add' ? addFavoriteText : removeFavoriteText}
+              </button>
+            </div>
+          </>
+        )}
       </Card>
     </li>
   );
