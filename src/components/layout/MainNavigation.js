d86ef2f502ef3800ece5b7dc95048f55cbab7a14
Nacho <nachomocoroa@gmail.com>
2022-06-01 12:55:16 +0200

    Header functionalities on scroll

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,10 +1,46 @@
+import { useState, useEffect } from "react";
 import { ALL_MEETUP_PAGE, FAVORITES_PAGE, NEW_MEETUP_PAGE } from "./../../utils/constants";
 
 import classes from "./MainNavigation.module.css";
 
 export default function MainNavigation({ setPage }) {
+
+  const headerHeigh = 100;
+  const [position, setPosition] = useState(window.pageYOffset);
+  const [headerVisible, setHeaderVisible] = useState(true);
+  const [showTopBtn, setShowTopBtn] = useState(false);
+  const headerVisibleClasses = headerVisible ? classes.visible : classes.hidden;
+  const topBtnVisibleClass = showTopBtn ? classes.top_button_visible : '';
+
+  const scrollToTop = () =>{
+    window.scrollTo({
+      top: 0, 
+      behavior: 'smooth'
+    });
+  };
+
+  useEffect(()=> {
+    const handleScroll = () => {
+      let moving = window.pageYOffset;
+      setPosition(moving);
+      setHeaderVisible(position > moving);
+      setShowTopBtn(position > headerHeigh);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return(() => {
+      window.removeEventListener("scroll", handleScroll);
+    });
+  });
+
   return (
-    <header className={classes.header} data-test="navigation-header">
+    <header 
+      className={`${classes.header} ${headerVisibleClasses}`} 
+      data-test="navigation-header"
+    >
+      <button 
+        onClick={() => scrollToTop()} 
+        className={`${classes.top_button} ${topBtnVisibleClass}`}
+      ></button> 
       <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
